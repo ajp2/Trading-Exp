f8ee2b5fc4288a537e632470e724679c9196679e
AJP <arvind.puvi@gmail.com>
2019-03-16 19:55:45 +1100

    Return jwt token upon login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
+const secretOrKey = require("../../config/keys").secretOrKey;
+const jwt = require("jsonwebtoken");
 
 router.get("/", (req, res) => {
   res.send("users");
@@ -45,7 +47,19 @@ router.post("/login", (req, res) => {
 
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "Success!" });
+        const payload = {
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email,
+          username: user.username
+        };
+        jwt.sign(payload, secretOrKey, { expiresIn: 3600 }, (err, token) => {
+          res.json({
+            success: true,
+            token: "Bearer " + token
+          });
+        });
       } else {
         return res.status(400).json({ password: "Incorrect password" });
       }
