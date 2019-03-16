897be70cb722274e6b574ff0a268b4e5b95174de
AJP <arvind.puvi@gmail.com>
2019-03-16 14:14:44 +1100

    Incorporate express router for api/users

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 
+const users = require("./routes/api/users");
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to mongoDB"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
+  debugger;
   res.send("testing");
 });
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
