520c778d718505fa3dcd244aff86cc4d2aae2dd1
AJP <arvind.puvi@gmail.com>
2019-03-20 17:42:03 +1100

    Add authentication over multiple sessions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,34 @@ import ReactDOM from "react-dom";
 import Root from "./components/root";
 import configureStore from "./store/store";
 
+import { setAuthToken } from "./util/session_api_util";
+import jwt_decode from "jwt-decode";
+import { logout } from "./actions/session_actions";
+
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
+  let store;
+
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = {
+      session: {
+        isAuthenticated: true,
+        authoriseSignIn: false,
+        user: decodedUser
+      }
+    };
+
+    store = configureStore(preloadedState);
+    const currentTime = Date.now() / 1000;
+
+    if (decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = "/login";
+    }
+  } else {
+    store = configureStore();
+  }
 
   ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 });
