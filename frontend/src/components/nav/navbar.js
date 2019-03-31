73798da7ad46267bac1638d76489bb5360c2c8e3
AJP <arvind.puvi@gmail.com>
2019-03-31 18:47:29 +1100

    Add searchbar to nav

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -5,8 +5,12 @@ import "./navbar.css";
 export class NavBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchBar: ""
+    };
 
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -18,10 +22,15 @@ export class NavBar extends Component {
     this.props.history.push("/");
   }
 
+  handleChange(e) {
+    this.setState({ searchBar: e.target.value });
+    console.log(e.target.value);
+  }
+
   displayLinks() {
     if (this.props.loggedIn) {
       return (
-        <div>
+        <div className="links">
           <Link to="/portfolio">Home</Link>
           <Link to="/trades">Trades</Link>
           <button onClick={this.handleLogout}>Logout</button>
@@ -48,12 +57,23 @@ export class NavBar extends Component {
   render() {
     return (
       <header className="navbar">
-        <Link to={this.props.loggedIn ? "/portfolio" : "/"} className="brand">
-          <img src={require("../../images/logo.png")} alt="" />
-          <h1>
-            Trading<span className="brand-text">Exp</span>
-          </h1>
-        </Link>
+        <div className="menu-left">
+          <Link to={this.props.loggedIn ? "/portfolio" : "/"} className="brand">
+            <img src={require("../../images/logo.png")} alt="" />
+            <h1>
+              Trading<span className="brand-text">Exp</span>
+            </h1>
+          </Link>
+          {this.props.loggedIn ? (
+            <input
+              type="text"
+              placeholder="search"
+              onChange={this.handleChange}
+            />
+          ) : (
+            false
+          )}
+        </div>
         {this.displayLinks()}
         <div className="hamburger-menu">
           <div />
