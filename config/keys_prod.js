b2212bf4d14811ac66c93e8cd6a05ba6c9b23f02
AJP <arvind.puvi@gmail.com>
2019-04-07 19:42:55 +1000

    Add newsAPI to prod keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
-  alphaVantage: process.env.ALPHA_VANTAGE
+  alphaVantage: process.env.ALPHA_VANTAGE,
+  newsAPI: process.env.NEWS_API
 };
