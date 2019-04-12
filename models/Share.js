b227c3d5eb45b064973ba623a9c88fcde77b03b3
AJP <arvind.puvi@gmail.com>
2019-04-13 00:05:36 +1000

    Create Share model

---
+++ models/Share.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ShareSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  ticker: {
+    type: String,
+    required: true
+  },
+  owned: {
+    type: Number,
+    default: 0
+  },
+  watchlist: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Share = mongoose.model("shares", ShareSchema);
+module.exports = Share;
