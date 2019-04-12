4d27417d37c23dc662c0a81f735a228d2019abab
AJP <arvind.puvi@gmail.com>
2019-04-13 00:10:11 +1000

    Create Trade model

---
+++ models/Trade.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TradeSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  ticker: {
+    type: String,
+    required: true
+  },
+  company: {
+    type: String,
+    required: true
+  },
+  buy: {
+    type: Boolean,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Trade = mongoose.model("trades", TradeSchema);
+module.exports = Trade;
