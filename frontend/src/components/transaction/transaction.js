8acf82e5e635f62c037ab9b4aaa2e3bd3684bb55
AJP <arvind.puvi@gmail.com>
2019-04-12 23:50:31 +1000

    Add more functionality and styling to transaction component

--- frontend/src/components/transaction/transaction.js
+++ frontend/src/components/transaction/transaction.js
@@ -7,13 +7,22 @@ export class Transaction extends Component {
 
     // activeButton: true - buy, false - sell
     this.state = {
-      activeButton: true
+      activeButton: true,
+      shares: "",
+      totalValue: 0,
+      errors: ""
     };
 
     this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkErrors = this.checkErrors.bind(this);
   }
 
   handleButtonClick(e) {
+    this.setState({ shares: "", totalValue: 0, errors: "" }, this.checkErrors);
+
+    // Button styling
     if (e.target.classList.contains("buy") && !this.state.activeButton) {
       e.target.classList.add("button-green");
       document.querySelector(".sell").classList.remove("button-red");
@@ -23,6 +32,38 @@ export class Transaction extends Component {
       document.querySelector(".buy").classList.remove("button-green");
       this.setState({ activeButton: false });
     }
+  }
+
+  handleChange(e) {
+    this.setState(
+      {
+        shares: e.target.value,
+        totalValue: this.props.latestPrice * e.target.value
+      },
+      this.checkErrors
+    );
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // get owned shares and add/subtract this.state.shares to that value
+    // if error caused by insufficient funds/shares, send error message
+  }
+
+  checkErrors() {
+    // TODO: set available funds & owned shares;
+    let errors;
+    if (this.state.activeButton && this.state.totalValue > 1000000) {
+      errors = "Insufficient funds";
+    } else if (!this.state.activeButton && this.state.shares > 0) {
+      errors = "You do not own that many shares";
+    } else {
+      errors = "";
+      document.querySelector(".submit").disabled = false;
+    }
+
+    if (errors) document.querySelector(".submit").disabled = true;
+    this.setState({ errors });
   }
 
   render() {
@@ -39,25 +80,37 @@ export class Transaction extends Component {
           </p>
           <label>
             Shares
-            <input type="number" placeholder="0" />
+            <input
+              type="number"
+              placeholder="0"
+              value={this.state.shares}
+              onChange={this.handleChange}
+            />
           </label>
           <p>
             Market Price <span className="value">{this.props.latestPrice}</span>
           </p>
           <p>
-            {this.state.activeButton ? "Cost" : "Proceeds"}
-            <span className="value">100</span>
+            {this.state.activeButton ? "Total Cost" : "Total Proceeds"}
+            <span className="value">
+              {this.state.totalValue.toLocaleString()}
+            </span>
           </p>
           <p>
             Available Funds <span className="value">1,000,000</span>
           </p>
         </div>
 
-        {this.state.activeButton ? (
-          <button className="submit buy-button">Buy</button>
-        ) : (
-          <button className="submit sell-button">Sell</button>
-        )}
+        {this.state.errors ? <p>{this.state.errors}</p> : false}
+
+        <button
+          className={`submit ${
+            this.state.activeButton ? "buy-button" : "sell-button"
+          }`}
+          onSubmit={this.handleSubmit}
+        >
+          {this.state.activeButton ? "Buy" : "Sell"}
+        </button>
 
         {/* Check if in watchlist */}
         <button className="watchlist">Add to Watchlist</button>
